31f137e7693d722b0cda3ac9557b264d9a6ba75b
Sanny Sanoff <san@sysdate.com>
2014-10-23 19:20:41 +0300

    stuff

--- src/com/juickadvanced/imaging/ExtractImageURLFromHTML.java
+++ src/com/juickadvanced/imaging/ExtractImageURLFromHTML.java
@@ -11,6 +11,7 @@ public class ExtractImageURLFromHTML {
     public static String extractImageURLFromHTML(StringBuffer sb) {
         String html = sb.toString();
         String imageURL = null;
+        int ix;
         if (html.indexOf("gelbooru") != -1) {
             // <img src="http://cdn1.gelbooru.com//samples/1397/sample_8f31057149f27c1c211f587d8251cedb.jpg?1608984" id="image" ...
             int idimage = html.indexOf("id=\"image\"");
@@ -24,6 +25,16 @@ public class ExtractImageURLFromHTML {
                         html = html.substring(0, quote-1);
                         imageURL = html;
                     }
+                }
+            }
+        }
+        if ((ix = html.indexOf("meta property=\"og:image\"")) != -1) {
+            int contentIndex = html.indexOf("content=\"", ix);
+            if (contentIndex != -1) {
+                html = html.substring(contentIndex+9);
+                int src = html.indexOf('"');
+                if (src != -1) {
+                    imageURL = html.substring(0, src);
                 }
             }
         }
